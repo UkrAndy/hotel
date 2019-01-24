9e5dc23efddd735788fe838ce12fef8db790dba2
k1viz <k1vizenko@gmail.com>
2019-01-24 16:18:10 +0200

    Закінчив components

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import React, {Components} from "react";
+import HotelApp from "./hotelApp";
+
+const Components = (props)=>{
+    return(
+        <HotelApp {...props}></HotelApp>
+    )
+}
+
+export default Components;
